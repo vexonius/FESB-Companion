4b4ec1d39091d1aeae92d4db742b23810ce5e9b1
stipe-jurkovic <sjurko00@fesb.hr>
2023-11-17 22:15:02 +0100

    sampleslide to kotlin and fixed the xml-s a bit

--- app/src/main/java/com/tstudioz/fax/fme/fragments/SampleSlide.kt
+++ app/src/main/java/com/tstudioz/fax/fme/fragments/SampleSlide.kt
@@ -1,45 +1,38 @@
-package com.tstudioz.fax.fme.fragments;
+package com.tstudioz.fax.fme.fragments
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 /**
  * Created by amarthus on 17-Sep-17.
  */
-
-public class SampleSlide extends Fragment {
-
-    private static final String ARG_LAYOUT_RES_ID = "layoutResId";
-    private int layoutResId;
-
-    public static SampleSlide newInstance(int layoutResId) {
-        SampleSlide sampleSlide = new SampleSlide();
-
-        Bundle args = new Bundle();
-        args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
-        sampleSlide.setArguments(args);
-
-        return sampleSlide;
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID)) {
-            layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+class SampleSlide : Fragment() {
+    private var layoutResId = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null && requireArguments().containsKey(ARG_LAYOUT_RES_ID)) {
+            layoutResId = requireArguments().getInt(ARG_LAYOUT_RES_ID)
         }
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(layoutResId, container, false);
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutResId, container, false)
+    }
+
+    companion object {
+        private const val ARG_LAYOUT_RES_ID = "layoutResId"
+        fun newInstance(layoutResId: Int): SampleSlide {
+            val sampleSlide = SampleSlide()
+            val args = Bundle()
+            args.putInt(ARG_LAYOUT_RES_ID, layoutResId)
+            sampleSlide.arguments = args
+            return sampleSlide
+        }
     }
 }
