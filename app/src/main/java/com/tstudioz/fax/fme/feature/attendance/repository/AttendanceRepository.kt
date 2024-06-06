ad632d4ae278ce4ff9a9412cc2f311c63be6630f
stipe-jurkovic <sjurko00@fesb.hr>
2024-06-06 13:54:05 +0200

    changed the network call corutine thing to handle errors better

--- app/src/main/java/com/tstudioz/fax/fme/feature/attendance/repository/AttendanceRepository.kt
+++ app/src/main/java/com/tstudioz/fax/fme/feature/attendance/repository/AttendanceRepository.kt
@@ -7,6 +7,7 @@ import com.tstudioz.fax.fme.feature.attendance.parseAttendance
 import com.tstudioz.fax.fme.feature.attendance.services.AttendanceServiceInterface
 import com.tstudioz.fax.fme.models.NetworkServiceResult
 import com.tstudioz.fax.fme.models.data.User
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,37 +22,37 @@ class AttendanceRepository(
 
         when (val list = attendanceService.fetchAttendance(user)) {
             is NetworkServiceResult.PrisutnostResult.Success -> {
-                var error = false
+                var isSuccessful = true
                 val attendanceList = mutableListOf<List<Dolazak>>()
-                try {
-                    parseAttendList(list.data as String).map {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            when (val kolegijData = attendanceService.getDetailedPrisutnost(it.first)) {
-                                is NetworkServiceResult.PrisutnostResult.Success -> {
-                                    val attendOneKolegij =
-                                        parseAttendance(it.first, kolegijData.data as String, it.second)
-                                    attendanceList.add(attendOneKolegij)
-                                }
+                val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+                    println("Caught exception $throwable in CoroutineExceptionHandler")
+                    isSuccessful = false
+                }
+                parseAttendList(list.data as String).map {
+                    CoroutineScope(Dispatchers.IO).launch(exceptionHandler) {
+                        when (val kolegijData = attendanceService.getDetailedPrisutnost(it.first)) {
+                            is NetworkServiceResult.PrisutnostResult.Success -> {
+                                val attendOneKolegij =
+                                    parseAttendance(it.first, kolegijData.data as String, it.second)
+                                attendanceList.add(attendOneKolegij)
+                            }
 
-                                is NetworkServiceResult.PrisutnostResult.Failure -> {
-                                    error = true
-                                    return@launch
-                                }
+                            is NetworkServiceResult.PrisutnostResult.Failure -> {
+                                throw Exception("Error while fetching attendance data")
                             }
                         }
-                    }.forEach { it.join() }
-                } catch (e: Throwable) {
-                    error = true
-                }
-                if (error) {
-                    return NetworkServiceResult.PrisutnostResult.Failure(
+                    }
+                }.forEach { it.join() }
+
+                return if (isSuccessful) {
+                    NetworkServiceResult.PrisutnostResult.Failure(
                         Throwable("Error while fetching attendance data")
                     )
+                } else {
+                    NetworkServiceResult.PrisutnostResult.Success(
+                        attendanceList.sortedBy { it[0].predmet }.sortedBy { it[0].semestar }
+                    )
                 }
-                return NetworkServiceResult.PrisutnostResult.Success(
-                    attendanceList.sortedBy { it[0].predmet }
-                    .sortedBy { it[0].semestar }
-                )
             }
 
             is NetworkServiceResult.PrisutnostResult.Failure -> {
