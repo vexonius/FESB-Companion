c9f022243d7867c7c311a81519a4dbe2571965fd
stipe-jurkovic <115549802+stipe-jurkovic@users.noreply.github.com>
2024-06-11 00:07:09 +0200

    Update app/src/main/java/com/tstudioz/fax/fme/database/models/MonthData.kt
    Co-authored-by: vexonius <tino@etino.dev>

--- app/src/main/java/com/tstudioz/fax/fme/database/models/MonthData.kt
+++ app/src/main/java/com/tstudioz/fax/fme/database/models/MonthData.kt
@@ -5,4 +5,8 @@ import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import java.time.DayOfWeek
 import java.time.YearMonth
 
-data class MonthData(val currentMonth: YearMonth, val startMonth: YearMonth, val endMonth: YearMonth, val firstDayOfWeek: DayOfWeek)
+data class MonthData(
+    val currentMonth: YearMonth, 
+    val startMonth: YearMonth, 
+    val endMonth: YearMonth, 
+    val firstDayOfWeek: DayOfWeek)
