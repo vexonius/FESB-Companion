5c4926e6232ad1d04ccad4c311006bb51859c87a
vexonius <tino@etino.dev>
2024-10-24 23:51:17 +0200

    Created router class with all conformances

---
+++ app/src/main/java/com/tstudioz/fax/fme/routing/Router.kt
@@ -0,0 +1,72 @@
+package com.tstudioz.fax.fme.routing
+
+import android.app.Activity
+import android.content.Intent
+import com.tstudioz.fax.fme.feature.login.view.LoginActivity
+import com.tstudioz.fax.fme.feature.settings.SettingsActivity
+import com.tstudioz.fax.fme.view.activities.MainActivity
+import java.lang.ref.WeakReference
+
+class Router: LoginRouter, HomeRouter, SettingsRouter {
+
+    private var activity: WeakReference<Activity>? = null
+
+    override fun register(activity: Activity) {
+        this.activity = WeakReference(activity)
+    }
+
+    override fun routeToHome() {
+        val activity = activity?.get() ?: return
+
+        activity.startActivity(Intent(activity, MainActivity::class.java))
+        activity.finish()
+    }
+
+    override fun routeToLogin() {
+        val activity = activity?.get() ?: return
+
+        activity.startActivity(Intent(activity, LoginActivity::class.java))
+        activity.finish()
+    }
+
+    override fun routeToSettings() {
+        val activity = activity?.get() ?: return
+
+        activity.startActivity(Intent(activity, SettingsActivity::class.java))
+    }
+
+    override fun popToHome() {
+        val activity = activity?.get() ?: return
+
+        activity.finish()
+    }
+
+
+}
+
+interface LoginRouter {
+
+    fun register(activity: Activity)
+
+    fun routeToHome()
+
+}
+
+interface HomeRouter {
+
+    fun register(activity: Activity)
+
+    fun routeToLogin()
+
+    fun routeToSettings()
+
+}
+
+interface SettingsRouter {
+
+    fun register(activity: Activity)
+
+    fun popToHome()
+
+    fun routeToLogin()
+}
