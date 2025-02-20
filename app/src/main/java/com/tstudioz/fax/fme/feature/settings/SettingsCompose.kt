ecf55e8426adb0ddbf6641d39360a237b75cbf8b
stipe-jurkovic <sjurko00@fesb.hr>
2025-02-20 21:46:36 +0100

    added scroll to the settings

--- app/src/main/java/com/tstudioz/fax/fme/feature/settings/SettingsCompose.kt
+++ app/src/main/java/com/tstudioz/fax/fme/feature/settings/SettingsCompose.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -87,7 +89,7 @@ fun SettingsCompose(viewModel: SettingsViewModel = koinViewModel(), router: Sett
             })
         {
             Column(
-                modifier = Modifier.padding(it)
+                modifier = Modifier.padding(it).verticalScroll(rememberScrollState())
             ) {
                 CategoryTitle(title = stringResource(id = R.string.category_user))
                 SettingsItem(
