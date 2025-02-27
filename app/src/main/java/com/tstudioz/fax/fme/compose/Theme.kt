abc40761197dcdb4acef0944cebf79dd070625c6
vexonius <tino@etino.dev>
2025-02-27 17:15:34 +0100

    Defined colors for the app's dark theme

--- app/src/main/java/com/tstudioz/fax/fme/compose/Theme.kt
+++ app/src/main/java/com/tstudioz/fax/fme/compose/Theme.kt
@@ -4,73 +4,71 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 
-private val LightColors = lightColorScheme(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    primaryContainer = md_theme_light_primaryContainer,
-    onPrimaryContainer = md_theme_light_onPrimaryContainer,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    secondaryContainer = md_theme_light_secondaryContainer,
-    onSecondaryContainer = md_theme_light_onSecondaryContainer,
-    tertiary = md_theme_light_tertiary,
-    onTertiary = md_theme_light_onTertiary,
-    tertiaryContainer = md_theme_light_tertiaryContainer,
-    onTertiaryContainer = md_theme_light_onTertiaryContainer,
-    error = md_theme_light_error,
-    errorContainer = md_theme_light_errorContainer,
-    onError = md_theme_light_onError,
-    onErrorContainer = md_theme_light_onErrorContainer,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
-    surfaceVariant = md_theme_light_surfaceVariant,
-    onSurfaceVariant = md_theme_light_onSurfaceVariant,
-    outline = md_theme_light_outline,
-    inverseOnSurface = md_theme_light_inverseOnSurface,
-    inverseSurface = md_theme_light_inverseSurface,
-    inversePrimary = md_theme_light_inversePrimary,
-    surfaceTint = md_theme_light_surfaceTint,
-    outlineVariant = md_theme_light_outlineVariant,
-    scrim = md_theme_light_scrim,
-)
-
+//private val LightColors = lightColorScheme(
+//    primary = theme_light_primary,
+//    onPrimary = theme_light_onPrimary,
+//    primaryContainer = theme_light_primaryContainer,
+//    onPrimaryContainer = theme_light_onPrimaryContainer,
+//    secondary = theme_light_secondary,
+//    onSecondary = theme_light_onSecondary,
+//    secondaryContainer = theme_light_secondaryContainer,
+//    onSecondaryContainer = theme_light_onSecondaryContainer,
+//    tertiary = theme_light_tertiary,
+//    onTertiary = theme_light_onTertiary,
+//    tertiaryContainer = theme_light_tertiaryContainer,
+//    onTertiaryContainer = theme_light_onTertiaryContainer,
+//    error = theme_light_error,
+//    errorContainer = theme_light_errorContainer,
+//    onError = theme_light_onError,
+//    onErrorContainer = theme_light_onErrorContainer,
+//    background = theme_light_background,
+//    onBackground = theme_light_onBackground,
+//    surface = theme_light_surface,
+//    onSurface = theme_light_onSurface,
+//    surfaceVariant = theme_light_surfaceVariant,
+//    onSurfaceVariant = theme_light_onSurfaceVariant,
+//    outline = theme_light_outline,
+//    inverseOnSurface = theme_light_inverseOnSurface,
+//    inverseSurface = theme_light_inverseSurface,
+//    inversePrimary = theme_light_inversePrimary,
+//    surfaceTint = theme_light_surfaceTint,
+//    outlineVariant = theme_light_outlineVariant,
+//    scrim = theme_light_scrim,
+//)
 
 private val DarkColors = darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    secondaryContainer = md_theme_dark_secondaryContainer,
-    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
-    onError = md_theme_dark_onError,
-    onErrorContainer = md_theme_dark_onErrorContainer,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-    surfaceVariant = md_theme_dark_surfaceVariant,
-    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
-    inverseSurface = md_theme_dark_inverseSurface,
-    inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
-    outlineVariant = md_theme_dark_outlineVariant,
-    scrim = md_theme_dark_scrim,
+    primary = theme_dark_primary,
+    onPrimary = theme_dark_onPrimary,
+    primaryContainer = theme_dark_primaryContainer,
+    onPrimaryContainer = theme_dark_onPrimaryContainer,
+    secondary = theme_dark_secondary,
+    onSecondary = theme_dark_onSecondary,
+    secondaryContainer = theme_dark_secondaryContainer,
+    onSecondaryContainer = theme_dark_onSecondaryContainer,
+    tertiary = theme_dark_tertiary,
+    onTertiary = theme_dark_onTertiary,
+    tertiaryContainer = theme_dark_tertiaryContainer,
+    onTertiaryContainer = theme_dark_onTertiaryContainer,
+    error = theme_dark_error,
+    errorContainer = theme_dark_errorContainer,
+    onError = theme_dark_onError,
+    onErrorContainer = theme_dark_onErrorContainer,
+    background = theme_dark_background,
+    onBackground = theme_dark_onBackground,
+    surface = theme_dark_surface,
+    onSurface = theme_dark_onSurface,
+    surfaceVariant = theme_dark_surfaceVariant,
+    onSurfaceVariant = theme_dark_onSurfaceVariant,
+    outline = theme_dark_outline,
+    inverseOnSurface = theme_dark_inverseOnSurface,
+    inverseSurface = theme_dark_inverseSurface,
+    inversePrimary = theme_dark_inversePrimary,
+    surfaceTint = theme_dark_surfaceTint,
+    outlineVariant = theme_dark_outlineVariant,
+    scrim = theme_dark_scrim,
 )
 
 @Composable
@@ -78,16 +76,14 @@ fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    /*val colors = if (!useDarkTheme) {
-        LightColors
-    } else {
-        DarkColors
-    }*/
-
-    val colors = DarkColors
+//    val colors = if (!useDarkTheme) {
+//        LightColors
+//    } else {
+//        DarkColors
+//    }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = DarkColors,
         content = content,
         typography = Typography(),
         shapes = Shapes,
