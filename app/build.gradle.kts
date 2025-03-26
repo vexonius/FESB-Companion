bc29517daadfbb622c434ed044e57f644bf8a306
stipe-jurkovic <sjurko00@fesb.hr>
2025-03-26 23:33:25 +0100

    chore: remove commented-out dependencies for accompanist pager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,8 +147,6 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
 
     //horizontal pager
-    /*implementation("com.google.accompanist:accompanist-pager:0.37.2")
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.37.2")*/
     implementation("com.tbuonomo:dotsindicator:5.1.0")
 
 
