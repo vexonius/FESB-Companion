dd82116d25dbb36c9cd8391ddebe7b975bbccc12
stipe-jurkovic <sjurko00@fesb.hr>
2025-04-02 01:03:50 +0200

    Remove commented-out Realm implementation from TimeTableDao

--- app/src/main/java/com/tstudioz/fax/fme/feature/timetable/dao/TimeTableDao.kt
+++ app/src/main/java/com/tstudioz/fax/fme/feature/timetable/dao/TimeTableDao.kt
@@ -7,37 +7,6 @@ import androidx.room.Query
 import com.tstudioz.fax.fme.database.models.EventRoom
 import kotlinx.coroutines.flow.Flow
 
-/*class TimeTableDao(private val dbManager: DatabaseManagerInterface) : TimeTableDaoInterface {
-
-    override suspend fun insert(classes: List<Event>) {
-        val realm = Realm.open(dbManager.getDefaultConfiguration())
-
-        realm.write {
-            val oldClasses = this.query<EventRoom>().find()
-            this.delete(oldClasses)
-            classes.forEach {
-                this.copyToRealm(toRoomObject(it), updatePolicy = UpdatePolicy.ALL)
-            }
-        }
-    }
-
-    override suspend fun getEvents(): List<Event> {
-        val realm = Realm.open(dbManager.getDefaultConfiguration())
-        return realm.query<EventRoom>().find().map { fromRoomObject(it) }
-    }
-
-    override suspend fun getEventsAsync(): Flow<List<Event>> {
-        val realm = Realm.open(dbManager.getDefaultConfiguration())
-        val events = realm.query(EventRoom::class)
-
-        return  events
-            .asFlow()
-            .map { it.list }
-            .map { results -> results.map { fromRoomObject(it) } }
-    }
-
-}*/
-
 @Dao
 interface TimeTableDao{
     @Query("DELETE FROM eventroom")
