e021bf65c03298475e287b284235d26177f803b7
stipe-jurkovic <sjurko00@fesb.hr>
2025-05-06 00:07:37 +0200

    feat: implement MealTimeContent composable and update meal display logic and change color and agg a bit of glow.

--- app/src/main/java/com/tstudioz/fax/fme/feature/iksica/compose/MeniIksica.kt
+++ app/src/main/java/com/tstudioz/fax/fme/feature/iksica/compose/MeniIksica.kt
@@ -1,5 +1,6 @@
 package com.tstudioz.fax.fme.feature.iksica.compose
 
+import android.graphics.Paint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,14 +21,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.tstudioz.fax.fme.R
+import com.tstudioz.fax.fme.compose.meniBackground
+import com.tstudioz.fax.fme.compose.meniGlow
 import com.tstudioz.fax.fme.feature.iksica.models.MenzaLocation
 import com.tstudioz.fax.fme.feature.menza.models.MealTime
 import com.tstudioz.fax.fme.feature.menza.models.MeniSpecial
@@ -39,19 +46,13 @@ fun MeniComposeIksica(meni: Pair<MenzaLocation, Menza?>?) {
     val menzaLocation = meni?.first
     val menies = meni?.second
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-    val mealModifier = Modifier
-        .padding(bottom = 16.dp)
-        .clip(RoundedCornerShape(15.dp))
-        .background(Color(0xFF101010))
-        .padding(24.dp, 8.dp)
-        .fillMaxWidth()
 
     Column(
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp))
-            .background(Color(0xFF202022))
+            .background(meniBackground)
             .padding(16.dp)
             .heightIn(min = screenHeight.times(0.7f))
             .fillMaxWidth()
@@ -67,29 +68,8 @@ fun MeniComposeIksica(meni: Pair<MenzaLocation, Menza?>?) {
             modifier = Modifier.padding(16.dp, 0.dp, 0.dp, 24.dp)
         )
         if (meni?.second?.dateFetched == meni?.second?.datePosted) {
-            Text(
-                text = "Ručak",
-                style = MaterialTheme.typography.displayMedium,
-                modifier = Modifier.padding(16.dp, 8.dp)
-            )
-            menies?.menies?.filter { it.mealTime == MealTime.LUNCH }?.forEach {
-                MeniItem(it, mealModifier)
-            }
-            menies?.meniesSpecial?.filter { it.mealTime == MealTime.LUNCH }?.let {
-                MeniSpecialIksica(it, mealModifier)
-            }
-            Text(
-                text = "Večera",
-                style = MaterialTheme.typography.displayMedium,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(16.dp, 8.dp)
-            )
-            menies?.menies?.filter { it.mealTime == MealTime.DINNER }?.forEach {
-                MeniItem(it, mealModifier)
-            }
-            menies?.meniesSpecial?.filter { it.mealTime == MealTime.DINNER }?.let {
-                MeniSpecialIksica(it, mealModifier)
-            }
+            menies?.let { MealTimeContent(it, MealTime.LUNCH) }
+            menies?.let { MealTimeContent(it, MealTime.DINNER) }
         } else {
             Column(
                 Modifier
@@ -109,6 +89,38 @@ fun MeniComposeIksica(meni: Pair<MenzaLocation, Menza?>?) {
             }
         }
     }
+}
+
+@Composable
+fun MealTimeContent(menies: Menza, mealTime: MealTime) {
+
+    val glowingRadius = 20.dp
+    val cornerRadius = 15.dp
+    val mealModifier = Modifier
+        .padding(bottom = 16.dp)
+        .glow(meniGlow, cornerRadius, glowingRadius)
+        .clip(RoundedCornerShape(cornerRadius))
+        .background(MaterialTheme.colorScheme.background)
+        .padding(24.dp, 8.dp)
+        .fillMaxWidth()
+    Row(
+        Modifier
+            .padding(bottom = 16.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = if (mealTime == MealTime.LUNCH) stringResource(R.string.lunch_title)
+            else stringResource(R.string.dinner_title),
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+    }
+    menies.menies.filter { it.mealTime == mealTime }.forEach {
+        MeniItem(it, mealModifier)
+    }
+    MeniSpecialIksica(menies.meniesSpecial.filter { it.mealTime == mealTime }, mealModifier)
 }
 
 @Composable
@@ -203,5 +215,42 @@ fun MeniTextIksica(text: String, type: String, divider: Boolean = true) {
         if (divider) {
             HorizontalDivider(color = MaterialTheme.colorScheme.outline, thickness = 1.dp)
         }
+    }
+}
+
+fun Modifier.glow(
+    color: Color,
+    cornersRadius: Dp = 10.dp,
+    glowingRadius: Dp = 10.dp,
+): Modifier = drawBehind {
+    val canvasSize = size
+    drawContext.canvas.nativeCanvas.apply {
+        drawRoundRect(
+            0f,
+            0f,
+            canvasSize.width, canvasSize.height,
+            cornersRadius.toPx(), cornersRadius.toPx(),
+            Paint().apply {
+                isAntiAlias = true
+                setShadowLayer(glowingRadius.toPx(), 0f, 0f, color.toArgb())
+            })
+        drawRoundRect(
+            0f,
+            0f,
+            canvasSize.width, canvasSize.height,
+            cornersRadius.toPx(), cornersRadius.toPx(),
+            Paint().apply {
+                isAntiAlias = true
+                setShadowLayer((glowingRadius / 2).toPx(), 0f, 0f, color.toArgb())
+            })
+        drawRoundRect(
+            0f,
+            0f,
+            canvasSize.width, canvasSize.height,
+            cornersRadius.toPx(), cornersRadius.toPx(),
+            Paint().apply {
+                isAntiAlias = true
+                setShadowLayer((glowingRadius / 4).toPx(), 0f, 0f, color.toArgb())
+            })
     }
 }
