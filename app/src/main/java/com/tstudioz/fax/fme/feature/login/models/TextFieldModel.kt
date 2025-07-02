27e0e3d9f8b29640d8cb1a291f300bef1c1f290b
stipe-jurkovic <115549802+stipe-jurkovic@users.noreply.github.com>
2025-07-02 15:17:53 +0200

    Dependencies update (#80)
    * added a libs.versions.toml moved all implementations into it, updated everything, kotlin is updated only to 2.0.20 because of realm
    
    * add stability configuration file
    
    * added autofill for login
    
    * update build configuration to use KSP and adjust clean task

--- app/src/main/java/com/tstudioz/fax/fme/feature/login/models/TextFieldModel.kt
+++ app/src/main/java/com/tstudioz/fax/fme/feature/login/models/TextFieldModel.kt
@@ -3,6 +3,7 @@ package com.tstudioz.fax.fme.feature.login.models
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.autofill.ContentType
 import androidx.lifecycle.MutableLiveData
 
 data class TextFieldModel(
@@ -11,5 +12,6 @@ data class TextFieldModel(
     val keyboardOptions: KeyboardOptions = KeyboardOptions(),
     val keyboardActions: KeyboardActions = KeyboardActions(),
     val textHidden: MutableLiveData<Boolean>? = null,
-    val trailingIcon: @Composable() (() -> Unit)? = null
+    val trailingIcon: @Composable() (() -> Unit)? = null,
+    val contentType: ContentType
 )
